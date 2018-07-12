fdc4b743888a7341654a826e18e671e3ae2f1b29
Eric Steen <eric.steen@und.edu>
2018-07-12 17:16:42 -0500

    First

--- anagramSolver.py
+++ anagramSolver.py
@@ -1,6 +1,31 @@
 import os
+from multiset import BaseMultiset
 cwd = os.getcwd()
-sl = open(cwd + 'sortedList')
+sl = open(cwd + '/sortedList')
+anagram = input("Enter Anagram: ")
+anagramSet = BaseMultiset.__init__(anagram)
+anagramLen = len(anagram)
 
-for words in sortedList(len(words) >= len(letters) )
-	for letters in words
+contins = True
+
+#print list(anagramSet)
+
+def isSublist(sublist,mainlist):
+	for item in sublist:
+			contains = True
+			if sublist.issubset(mainlist):
+				contains = True
+			else:
+				contains = False
+				break
+	return contains
+
+
+for line in sl:
+	if len(line) > anagramLen:
+		break
+	#print line
+	lineSet = multiset(line)
+	if isSublist(anagramSet,lineSet) is True:
+		print( '\t' + line)
+
